2d08b99558d8d8a46d2354441fabfd11c55611ea
Yohan Chalier <yohan@chalier.fr>
2021-09-20 13:20:17 +0200

    oscilloscope: added frequency detection

--- oscilloscope/display.py
+++ oscilloscope/display.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot
 import matplotlib.animation
 import threading
 import serial
+import struct
 
 
 OPERATING_VOLTAGE = 5
@@ -14,27 +15,44 @@ WINDOW = [0] * WINSIZE
 TIMES = [1000. * i / SAMPLING_FREQUENCY for i in range(WINSIZE)]
 FIGURE = matplotlib.pyplot.figure()
 AXIS = FIGURE.add_subplot(1, 1, 1)
+FREQUENCY = 0
 
 
 def animate(_):
     global AXIS
     global WINDOW
     global TIMES
+    global FREQUENCY
     AXIS.clear()
     AXIS.plot(TIMES, WINDOW, "+-", linewidth=.5)
     AXIS.set_ylabel("Voltage (V)")
     AXIS.set_xlabel("Time (ms)")
+    AXIS.set_title("Detected Frequency: %.2f Hz" % FREQUENCY)
 
 
 class SamplerThread(threading.Thread):
 
     def run(self):
         global WINDOW
+        global FREQUENCY
         arduino = serial.Serial("COM3", 9600)
+        consecutive = 0
+        delimiter = 42
         while True:
-            b = int.from_bytes(arduino.read(1), byteorder="little")
-            WINDOW.pop(0)
-            WINDOW.append(float(b) / MAX_RESOLUTION * OPERATING_VOLTAGE)
+            x = int.from_bytes(arduino.read(1), byteorder="little")
+            if x == delimiter:
+                consecutive += 1
+            else:
+                consecutive = 0
+            if consecutive >= 5:
+                consecutive = 0
+                sample_size = int.from_bytes(arduino.read(2), byteorder="little")
+                FREQUENCY = struct.unpack("f", arduino.read(4))[0]
+                print("Received packet: %d, %.2f Hz" % (sample_size, FREQUENCY))
+                for _ in range(sample_size):
+                    b = int.from_bytes(arduino.read(1), byteorder="little")
+                    WINDOW.pop(0)
+                    WINDOW.append(float(b) / MAX_RESOLUTION * OPERATING_VOLTAGE)         
 
 
 if __name__ == "__main__":
--- oscilloscope/sampler/sampler.ino
+++ oscilloscope/sampler/sampler.ino
@@ -1,9 +1,12 @@
 #define WAVE_IN A0
-#define SAMPLE_SIZE 128
+#define SAMPLE_SIZE 512
 #define SAMPLE_FREQUENCY 4096 // Hz
+#define DELIMITER 42
+#define DELIMITER_LENGTH 5
 
-int sample[SAMPLE_SIZE];
-unsigned long sample_period = int(1000000 / SAMPLE_FREQUENCY);
+int samples[SAMPLE_SIZE];
+unsigned long sampling_period = int(1000000. / SAMPLE_FREQUENCY);
+const int sample_size = SAMPLE_SIZE;
 
 void setup()
 {
@@ -13,25 +16,80 @@ void setup()
 
 void loop()
 {
-  if (Serial.availableForWrite() > 2)
+  acquire_samples();
+  float frequency = compute_frequency();
+  for (int i = 0; i < DELIMITER_LENGTH; i++)
   {
-    record_samples();
-    for (int i = 0; i < SAMPLE_SIZE; i++)
-    {
-      Serial.write((byte)round(sample[i] / 4));
-    }
+    Serial.write((byte)DELIMITER);
+  }
+  Serial.write((byte *)&sample_size, 2);
+  Serial.write((byte *)&frequency, 4);
+  for (int i = 0; i < SAMPLE_SIZE; i++)
+  {
+    Serial.write((byte)round(samples[i] / 4));
   }
 }
 
-void record_samples()
+void acquire_samples()
 {
   for (int i = 0; i < SAMPLE_SIZE; i++)
   {
     unsigned long now = micros();
-    sample[i] = analogRead(WAVE_IN);
-    while (micros() - now < sample_period)
+    samples[i] = analogRead(WAVE_IN);
+    while (micros() - now < sampling_period)
     {
       // pass
     }
   }
+}
+
+int count_crossings_rising(int frontier)
+{
+  int crossings = 0;
+  for (int i = 0; i < SAMPLE_SIZE - 1; i++)
+  {
+    if (samples[i] <= frontier && samples[i + 1] >= frontier)
+    {
+      crossings++;
+    }
+  }
+  return crossings;
+}
+
+void count_crossings(int frontier, int &rising, int &falling)
+{
+  rising = 0;
+  falling = 0;
+  for (int i = 0; i < SAMPLE_SIZE - 1; i++)
+  {
+    if (samples[i] <= frontier && samples[i + 1] >= frontier)
+    {
+      rising++;
+    }
+    if (samples[i] >= frontier && samples[i + 1] <= frontier)
+    {
+      falling++;
+    }
+  }
+}
+
+float compute_frequency()
+{
+  int min = 1024;
+  int max = 0;
+  for (int i = 0; i < SAMPLE_SIZE; i++)
+  {
+    if (samples[i] > max)
+    {
+      max = samples[i];
+    }
+    if (samples[i] < min)
+    {
+      min = samples[i];
+    }
+  }
+  int rising, falling;
+  count_crossings((max + min) / 2, rising, falling);
+  int crossings = (rising + falling) / 2;
+  return (float)crossings / (float)(SAMPLE_SIZE * (float)sampling_period / 1000000.);
 }
