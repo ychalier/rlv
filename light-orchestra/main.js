7612cd9e01a7a54cec3d8b0b20108203f6c9b822
Yohan Chalier <yohan@chalier.fr>
2021-09-24 17:45:27 +0200

    light-orchestra: updates

--- light-orchestra/main.js
+++ light-orchestra/main.js
@@ -1,31 +1,70 @@
 const WEBSOCKET_URL = "ws://atelier-mediatheque.rlv.eu/wst";
+var SLAVES = [];
 
 var socket;
 
 function setupMaster() {
-    socket = new WebSocket("ws://atelier-mediatheque.rlv.eu/wst");
+    socket = new WebSocket(WEBSOCKET_URL);
     socket.onopen = function(event) {
         socket.send(JSON.stringify({
-            cmd: "admin",
-            arg: "admin"
+            cmd: "master",
+            arg: "master"
         }));
     };
     socket.onmessage = function(event) {
-        console.log("Received message:", JSON.parse(event.data));
+        let message = JSON.parse(event.data);
+        console.log("Received message:", message);
+        if (message.cmd == "event") {
+            if (message.arg.type == "slave_connected") {
+                SLAVES.push(message.arg.content);
+                inflateSlaveList();
+            } else if (message.arg.type == "slave_disconnected") {
+                SLAVES = SLAVES.filter(slaveId => slaveId != message.arg.content);
+                inflateSlaveList();
+            }
+        }
     }
 
 }
 
+function inflateSlaveList() {
+    let container = document.getElementById("connected-slaves");
+    container.innerHTML = "";
+    SLAVES.forEach((slaveId, index) => {
+        let element = importTemplate("template-slave");
+        element.querySelector(".card-title").textContent = slaveId;
+        if (index == 0) element.querySelector(".btn-up").classList.add("disabled");
+        if (index == SLAVES.length - 1) element.querySelector(".btn-down").classList.add("disabled");
+        element.querySelector(".btn-up").addEventListener("click", () => {
+            let aux = SLAVES[index];
+            SLAVES[index] = SLAVES[index - 1];
+            SLAVES[index - 1] = aux;
+            inflateSlaveList();
+        });
+        element.querySelector(".btn-down").addEventListener("click", () => {
+            let aux = SLAVES[index];
+            SLAVES[index] = SLAVES[index + 1];
+            SLAVES[index + 1] = aux;
+            inflateSlaveList();
+        });
+        container.appendChild(element);
+    });
+}
+
 function setupSlave() {
-    socket = new WebSocket("ws://atelier-mediatheque.rlv.eu/wst");
+    socket = new WebSocket(WEBSOCKET_URL);
     socket.onopen = function(event) {
         socket.send(JSON.stringify({
-            cmd: "hello",
+            cmd: "slave",
             arg: null
         }));
     };
     socket.onmessage = function(event) {
-        console.log("Received message:", JSON.parse(event.data));
+        let message = JSON.parse(event.data);
+        console.log("Received message:", message);
+        if (message.cmd == "color") {
+            document.querySelector(".slave-container").style.background = "#" + message.arg;
+        }
     }
 }
 
@@ -34,7 +73,29 @@ function testBroadcast() {
         cmd: "broadcast",
         arg: {
             cmd: "color",
-            arg: "0f4q5e"
+            arg: document.getElementById("broadcast-color").value.substr(1)
         }
+    }));
+}
+
+function testTell(slaveId) {
+    socket.send(JSON.stringify({
+        cmd: "tell",
+        arg: {
+            slave: slaveId,
+            message: {
+                cmd: "color",
+                arg: "4f5a9d"
+            }
+        }
+    }));
+}
+
+
+function testList() {
+    SLAVES = [];
+    socket.send(JSON.stringify({
+        cmd: "list",
+        arg: null
     }));
 }
--- light-orchestra/server.py
+++ light-orchestra/server.py
@@ -4,62 +4,60 @@ import json
 
 
 PORT = 8765
-CLIENTS = dict()
-ADMIN = None
-ADMIN_PASSWORD = "admin"
+SLAVES = dict()
+MASTER = None
+MASTER_PASSWORD = "master"
 
 
-async def respond(websocket, status, message):
-    await websocket.send(json.dumps({"status": status, "message": message}))
+async def respond_status(websocket, status):
+    await websocket.send(json.dumps({"cmd": "status", "arg": status}, default=str))
+
+
+async def respond_event(websocket, event, content):
+    await websocket.send(json.dumps({"cmd": "event", "arg": {"type": event, "content": content}}, default=str))
 
 
 async def handle_command(websocket, cmd, arg):
-    global CLIENTS, ADMIN, ADMIN_PASSWORD
-    print(f"Handling command '{cmd}' from {websocket.id}")
-    if cmd == "hello":
-        CLIENTS[websocket.id] = websocket
-        await respond(websocket, "success", "")
-        if ADMIN is not None:
-            await respond(ADMIN, "event", {
-                "event": "client_connected",
-                "client": str(websocket.id)
-            })
-    elif cmd == "admin":
-        if arg == ADMIN_PASSWORD:
-            ADMIN = websocket
-            await respond(websocket, "success", {"clients": list(map(str, CLIENTS.keys()))})
+    global SLAVES, MASTER, MASTER_PASSWORD
+    print(f"{websocket.id}\tCMD\t{cmd}")
+    if cmd == "slave":
+        SLAVES[websocket.id] = websocket
+        await respond_status(websocket, "success")
+        if MASTER is not None:
+            await respond_event(MASTER, "slave_connected", websocket.id)
+    elif cmd == "master":
+        if arg == MASTER_PASSWORD:
+            MASTER = websocket
+            await respond_status(websocket, "success")
         else:
-            await respond(websocket, "error", "wrong password")
+            await respond_status(websocket, "error: wrong password")
     elif cmd == "broadcast":
-        if ADMIN is not None and websocket.id == ADMIN.id:
-            for client in CLIENTS.values():
-                await client.send(json.dumps(arg))
-            await respond(websocket, "success", "")
+        if MASTER is not None and websocket.id == MASTER.id:
+            for slave in SLAVES.values():
+                await slave.send(json.dumps(arg))
+            await respond_status(websocket, "success")
         else:
-            await respond(websocket, "error", "unauthorized")
+            await respond_status(websocket, "error: unauthorized")
     else:
-        await respond(websocket, "error", "unknown command")
+        await respond_status(websocket, "error: unknown command")
 
 
 async def ws_handler(websocket, path):
-    global CLIENTS, ADMIN
-    print(f"New client (id: {websocket.id} address: {websocket.remote_address}")
+    global MASTER, SLAVES
+    print(f"{websocket.id}\tCONNECTED")
     async for message in websocket:
         try:
             data = json.loads(message)
             await handle_command(websocket, data.get("cmd"), data.get("arg"))
         except json.decoder.JSONDecodeError:
-            await respond(websocket, "error", "invalid json")
-    print(f"Client {websocket.id} disconnected")
-    if websocket.id in CLIENTS:
-        del CLIENTS[websocket.id]
-        if ADMIN is not None:
-            await respond(ADMIN, "event", {
-                    "event": "client_disconnected",
-                    "client": str(websocket.id)
-                })
-    if websocket.id == ADMIN:
-        ADMIN = None
+            await respond_status(websocket, "error: invalid json")
+    print(f"{websocket.id}\tDISCONNECTED")
+    if websocket.id in SLAVES:
+        del SLAVES[websocket.id]
+        if MASTER is not None:
+            await respond_event(MASTER, "slave_disconnected", websocket.id)
+    if websocket.id == MASTER:
+        MASTER = None
 
 
 async def main():
@@ -67,5 +65,5 @@ async def main():
     async with websockets.serve(ws_handler, "localhost", PORT):
         await asyncio.Future()  # run forever
 
-asyncio.run(main())
 
+asyncio.run(main())
