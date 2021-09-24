4856f1d2b8a95f24e0aa865c604a604f6a6614a3
Yohan Chalier <yohan@chalier.fr>
2021-09-24 18:23:42 +0200

    light-orchestra: added gradient poc

--- light-orchestra/main.js
+++ light-orchestra/main.js
@@ -3,6 +3,63 @@ var SLAVES = [];
 
 var socket;
 
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+
+
+var GRADIENT = {
+
+    setup: {
+        keyframes: [{
+            value: 0,
+            color: "#f2627c"
+        }, {
+            value: 50,
+            color: "#277bcc"
+        }, {
+            value: 100,
+            color: "#f2627c"
+        }],
+        speed: .5,
+        spread: 2,
+    },
+    progress: 0
+
+}
+
+const INTERVAL_SPEED = 50;
+var gradientInterval = null;
+
+
+function blendColors(colA, colB, alpha) {
+    let rgbA = hexToRgb(colA);
+    let rgbB = hexToRgb(colB);
+    let blend = rgbToHex(
+        Math.floor((1 - alpha) * rgbA.r + alpha * rgbB.r),
+        Math.floor((1 - alpha) * rgbA.g + alpha * rgbB.g),
+        Math.floor((1 - alpha) * rgbA.b + alpha * rgbB.b)
+    );
+    return blend;
+}
+
+
 function setupMaster() {
     socket = new WebSocket(WEBSOCKET_URL);
     socket.onopen = function(event) {
@@ -10,10 +67,42 @@ function setupMaster() {
             cmd: "master",
             arg: "master"
         }));
+
+        gradientInterval = setInterval(() => {
+            // console.log("Hello from interval");
+            GRADIENT.progress += GRADIENT.setup.speed;
+            if (GRADIENT.progress > 100) GRADIENT.progress -= 100;
+            for (let i = 0; i < SLAVES.length; i++) {
+                let progress = GRADIENT.progress + GRADIENT.setup.spread * i;
+                if (progress > 100) progress -= 100;
+                for (let j = 0; j < GRADIENT.setup.keyframes.length - 1; j++) {
+                    if (GRADIENT.setup.keyframes[j].value <= progress && progress <= GRADIENT.setup.keyframes[j + 1].value) {
+                        let color = blendColors(
+                            GRADIENT.setup.keyframes[j].color,
+                            GRADIENT.setup.keyframes[j + 1].color,
+                            (progress - GRADIENT.setup.keyframes[j].value) / (GRADIENT.setup.keyframes[j + 1].value - GRADIENT.setup.keyframes[j].value)
+                        );
+                        socket.send(JSON.stringify({
+                            cmd: "tell",
+                            arg: {
+                                slave: SLAVES[i],
+                                message: {
+                                    cmd: "color",
+                                    arg: color.substr(1)
+                                }
+                            }
+                        }));
+                        break;
+                    }
+                }
+
+            }
+        }, INTERVAL_SPEED);
+
     };
     socket.onmessage = function(event) {
         let message = JSON.parse(event.data);
-        console.log("Received message:", message);
+        // console.log("Received message:", message);
         if (message.cmd == "event") {
             if (message.arg.type == "slave_connected") {
                 SLAVES.push(message.arg.content);
@@ -61,7 +150,7 @@ function setupSlave() {
     };
     socket.onmessage = function(event) {
         let message = JSON.parse(event.data);
-        console.log("Received message:", message);
+        // console.log("Received message:", message);
         if (message.cmd == "color") {
             document.querySelector(".slave-container").style.background = "#" + message.arg;
         }
